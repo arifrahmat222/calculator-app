89cdd857f264b9294aac91c6320e5327fb16a2d0
arifrahmat222 <arifrahmathidayatullah14@gmail.com>
2020-03-09 16:56:23 +0700

    calculator

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,16 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
+		
 	
 	}
+	ststic int Prnambahan(int a, int b)
+	{
+		return a + b;
+	}
+	ststic int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+	
 }
